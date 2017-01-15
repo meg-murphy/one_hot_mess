4b552f3ae900a4282197be57ec160dbfbaa58c4b
meg-murphy <meghan.murphy.usc@gmail.com>
2017-01-15 11:14:24 -0800

    switch to page layout

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # devise_for :users
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/", to: "recipes#index"
+
   resources :recipes
   resources :categories
   resources :favorites
@@ -9,6 +11,7 @@ Rails.application.routes.draw do
 
   post "recipes/:id/to_make", to: "user_recipes#make"
   get "recipes/:id/to_make", to: "user_recipes#make_progress"
+
 
 
   # get "/users/:id", to: "users#show"
